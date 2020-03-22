3b187dc07f185fd3bbd5554b3340135df6dbae29
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-03-22 18:53:45 -0500

    set up song class

---
+++ Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnythingButCountry
+{
+    public class Song
+    {
+        public  string Artist { get; set; }
+        public  string Name { get; set; }
+
+        public Song(string artist, string name)
+        {
+            this.Artist = artist;
+            this.Name = name;
+
+
+             var goodSongs = new List<string>(){};
+
+            var allSongs = new List<string>() {"Gorilla Pimp","Chicken Head" };
+            allSongs.Add("Sippin on Syrup");
+
+        }
+    }
+}
