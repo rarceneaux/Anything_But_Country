fa8f0f3cc000eb7f3ed3701250314529eca5be47
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-03-22 21:27:32 -0500

    WIP

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace AnythingButCountry
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var test = new Song("Project Pat","Don't save her");
+            Console.WriteLine();
         }
     }
 }
--- Song.cs
+++ Song.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace AnythingButCountry
 {
@@ -15,11 +16,20 @@ namespace AnythingButCountry
             this.Name = name;
 
 
-             var goodSongs = new List<string>(){};
-
-            var allSongs = new List<string>() {"Gorilla Pimp","Chicken Head" };
-            allSongs.Add("Sippin on Syrup");
-
+             /*var goodSongs = new List<string>(){};*/
+             var allSongs = new List<KeyValuePair<string,string>>();
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat","Keep it Hood"));
+             allSongs.Add(new KeyValuePair<string, string>("Juicy J","Hustle Til I Die"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "Gorilla Pimp"));
+             allSongs.Add(new KeyValuePair<string, string>("Kevin Gates", "MATA"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "Don't save her"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "Chicken Head"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "Ski Mask"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "North North"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "Aggravated Robbery"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "If you ain't from my hood"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "I Ain't going back to jail"));
+             allSongs.Add(new KeyValuePair<string, string>("Project Pat", "Sippin on some sizzurp"));
         }
     }
 }
